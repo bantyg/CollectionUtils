5d28be0957cf6b481da214a0a83960fd96a52ae6
Pooja <poojag@thoughtworks.com>
2015-02-28 15:27:03 +0530

    reduce is working fine

--- Collections.java
+++ Collections.java
@@ -1,39 +1,30 @@
 import java.util.*;
-interface ListMapper{
-	public int addFive(int number);
+
+interface ListMapper<E,K>{
+	public K addFive(E number);
 }
 
-interface FilterMapper{
-	public boolean isEven(int number);
+interface FilterMapper<E>{
+	public boolean isEven(E number);
 }
 
-class MMapper implements ListMapper{
-	public int addFive(int number){
-		return number+5;
-	}
-}
-
-class FMapper implements FilterMapper{
-	public boolean isEven(int number){
-		return number%2==0;
-	}
+interface ListReducer<E,K>{
+	public K addAll(E number,K initial);
 }
 
 public class Collections{
 
-	public static List<Integer> map(List<Integer> list, ListMapper listMapper){
-		listMapper = new MMapper();
-		List<Integer> result = new ArrayList<Integer>();
-		for(int element: list){
+	public static <E,K> List<K> map(List<E> list, ListMapper<E,K> listMapper){
+		List<K> result = new ArrayList<K>();
+		for(E element: list){
 			result.add(listMapper.addFive(element));
 		}
 		return result;
 	}
 
-	public static List<Integer> filter(List<Integer> list, FilterMapper fm){
-		fm = new FMapper();
-		List<Integer> result = new ArrayList<Integer>();
-		for(int element: list){
+	public static <E> List<E> filter(List<E> list, FilterMapper<E> fm){
+		List<E> result = new ArrayList<E>();
+		for(E element: list){
 			if(fm.isEven(element) == true){
 				result.add(element);
 			}
@@ -41,16 +32,24 @@ public class Collections{
 		return result;
 	}
 
-	public static void main(String[] args) {
-		FilterMapper fm = new FMapper();
-		List<Integer> numbers = new ArrayList<Integer>();
-		numbers.add(2);
-		numbers.add(3);
-		numbers.add(5);
-		numbers.add(6);
-		List<Integer> re = filter(numbers,fm);
-		System.out.println(re.get(0));
+	public static <E,K> K reduce(List<E> list, ListReducer<E,K> reducer, K initial){
+		K previous = initial;
+		for(E element: list){
+				previous = (reducer.addAll(element,previous));
+		}
+		return previous;	
 	}
+
+	// public static void main(String[] args) {
+	// 	FilterMapper fm = new FMapper();
+	// 	List<Integer> numbers = new ArrayList<Integer>();
+	// 	numbers.add(2);
+	// 	numbers.add(3);
+	// 	numbers.add(5);
+	// 	numbers.add(6);
+	// 	List<Integer> re = filter(numbers,fm);
+	// 	System.out.println(re.get(0));
+	// }
 
 
 }
--- CollectionsTest.java
+++ CollectionsTest.java
@@ -2,12 +2,29 @@ import org.junit.Test;
 import java.util.*;
 import static org.junit.Assert.*;
 
+class Functions implements ListMapper<Integer,Integer>, FilterMapper<Integer>,ListReducer<Integer,Integer>{
+
+		public Integer addFive(Integer number){
+			return number+5;
+		}
+
+		public boolean isEven(Integer number){
+			return number%2==0;
+		}
+
+		public Integer addAll(Integer number,Integer initial){
+			return number+initial;
+		}
+}
+
+
 public class CollectionsTest{
 
 	@Test
 	public void map_will_perform_given_operation_for_each_element_n_the_list(){
+
+		Functions f = new Functions();
 		List<Integer> l1 = new ArrayList<Integer>();
-		ListMapper lm = new MMapper();
 		l1.add(2);
 		l1.add(12);
 		l1.add(4);
@@ -17,13 +34,13 @@ public class CollectionsTest{
 		expected.add(17);
 		expected.add(9);
 		expected.add(10);
-		List<Integer> result = Collections.map(l1 , lm);
+		List<Integer> result = Collections.map(l1 , f);
 		assertEquals(expected.get(0),result.get(0));
 	}
 
 	@Test
 	public void map_returns_an_Integer_List_after_addint_five_to_ech_ele_of_given_list(){
-		ListMapper listMapper = new MMapper();
+		Functions f = new Functions();
 		List<Integer> numbers = new ArrayList<Integer>();
 		List<Integer> expected = new ArrayList<Integer>();
 		numbers.add(1);
@@ -32,7 +49,7 @@ public class CollectionsTest{
 		expected.add(6);
 		expected.add(7);
 		expected.add(8);
-		List<Integer> result = Collections.map(numbers,listMapper);
+		List<Integer> result = Collections.map(numbers,f);
 		assertEquals(expected.get(0),result.get(0));
 		assertEquals(expected.get(1),result.get(1));
 	}
@@ -40,24 +57,35 @@ public class CollectionsTest{
 	@Test
 	public void filter_will_perform_given_operation_for_each_element_n_the_list(){
 		List<Integer> l1 = new ArrayList<Integer>();
-		FilterMapper fm = new FMapper();
+		Functions f = new Functions();
 		l1.add(2);
 		l1.add(12);
 		l1.add(4);
 		l1.add(5);
-		List<Integer> result = Collections.filter(l1 , fm);
+		List<Integer> result = Collections.filter(l1 , f);
 		assertEquals((Integer)2,result.get(0));
 	}
 
 	@Test
 	public void filter_returns_an_Integer_List_after_checking_even_or_odd_on_the_given_list(){
-		FilterMapper filterMapper = new FMapper();
+		Functions f = new Functions();
 		List<Integer> numbers = new ArrayList<Integer>();
 		numbers.add(1);
 		numbers.add(2);
 		numbers.add(3);
-		List<Integer> result = Collections.filter(numbers,filterMapper);
+		List<Integer> result = Collections.filter(numbers,f);
 		assertEquals((Integer)2,result.get(0));
+	}
+
+	@Test
+	public void reduce_will_return_addition_of_all_elements_of_the_collection(){
+		Functions f = new Functions();
+		List<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		Integer result = Collections.reduce(numbers,f,0);
+		assertEquals(result,(Integer)6);
 	}
 }
 
