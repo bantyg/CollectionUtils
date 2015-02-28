62b01df5564352fb404d6d17d7bcbb30a5cc818c
Pooja <poojag@thoughtworks.com>
2015-02-28 12:35:51 +0530

    filter is working fine on the list of integer

--- Collections.java
+++ Collections.java
@@ -5,6 +5,11 @@ class ListMapper{
 	}
 }
 
+class FilterMapper{
+	public boolean isEven(int number){
+		return number%2==0;
+	}
+}
 
 public class Collections{
 
@@ -15,5 +20,27 @@ public class Collections{
 		}
 		return result;
 	}
+
+	public static List<Integer> filter(List<Integer> list, FilterMapper fm){
+		List<Integer> result = new ArrayList<Integer>();
+		for(int element: list){
+			if(fm.isEven(element) == true){
+				result.add(element);
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		FilterMapper fm = new FilterMapper();
+		List<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(5);
+		numbers.add(6);
+		List<Integer> re = filter(numbers,fm);
+		System.out.println(re.get(0));
+	}
+
 
 }
--- CollectionsTest.java
+++ CollectionsTest.java
@@ -22,7 +22,7 @@ public class CollectionsTest{
 	}
 
 	@Test
-	public void map_returns_an_Integer_List_after_Sqauring_on_the_given_list(){
+	public void map_returns_an_Integer_List_after_addint_five_to_ech_ele_of_given_list(){
 		ListMapper listMapper = new ListMapper();
 		List<Integer> numbers = new ArrayList<Integer>();
 		List<Integer> expected = new ArrayList<Integer>();
@@ -37,6 +37,28 @@ public class CollectionsTest{
 		assertEquals(expected.get(1),result.get(1));
 	}
 
+	@Test
+	public void filter_will_perform_given_operation_for_each_element_n_the_list(){
+		List<Integer> l1 = new ArrayList<Integer>();
+		FilterMapper fm = new FilterMapper();
+		l1.add(2);
+		l1.add(12);
+		l1.add(4);
+		l1.add(5);
+		List<Integer> result = Collections.filter(l1 , fm);
+		assertEquals((Integer)2,result.get(0));
+	}
+
+	@Test
+	public void filter_returns_an_Integer_List_after_checking_even_or_odd_on_the_given_list(){
+		FilterMapper filterMapper = new FilterMapper();
+		List<Integer> numbers = new ArrayList<Integer>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.add(3);
+		List<Integer> result = Collections.filter(numbers,filterMapper);
+		assertEquals((Integer)2,result.get(0));
+	}
 }
 
 
